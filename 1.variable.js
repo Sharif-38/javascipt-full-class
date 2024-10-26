097f5aa1dd839aac36fd8438f5a12ad0fb1f78c2
Sharif-38 <sharifulislam.tb@gmail.com>
2024-10-26 08:14:31 +0600

    add the new varialbe

---
+++ 1.variable.js
@@ -0,0 +1,19 @@
+
+
+// Variable : Variable has a container. We store the different types of data. Also when our need those data we can use the value
+
+// How can declare the Variable?
+/**
+ * 1. If we declare the variable we must use let, var, const fist.
+ * 2. Then variable name 
+ * 3. Variable value.
+ * 
+ */
+let name
+console.log(name) // undefined
+
+let fistName = 'Yusha bin Nun'
+console.log(fistName) // Yusha bin Nun
+
+let age = 30
+console.log(`My name is ${fistName}. I am ${age} old`)
