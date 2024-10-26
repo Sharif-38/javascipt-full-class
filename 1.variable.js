ce617e1ef9009ef54ee67b689a5b04a6a2efa5aa
Sharif-38 <sharifulislam.tb@gmail.com>
2024-10-26 09:19:14 +0600

    add all thing

--- 1.variable.js
+++ 1.variable.js
@@ -34,3 +34,36 @@ console.log(`My name is ${fistName}. I am ${age} old`)
  * 2. When we use let we can re-sing the value.
  * 3. const we can't re-sign the value. 
  */
+
+
+// Variable Scope: Variable has 3 type of Scope 1. Global Scope 2. Local Scope 3. Block Scope
+
+//Global Scope: Global Scope means we can access the variable out side or inside the function. We can access the console.log inside or out side function.
+
+let nameYou = "Yusha"
+
+function showName (){
+    console.log(`my name is ${nameYou}`) // my name is Yusha( you can declare in side the function)
+}
+
+showName ()
+ console.log(nameYou)// Also you can declare out side the function for Global Scope
+
+ // Local Scope or Function scope : Local Scope or Function Scope means we can access the variable only for in side out side we can't. And also we can't console.log in side the function not out side.
+
+function showAllThing () {
+    let number = 345
+    console.log(`This is the fist number ${number}`)
+}
+
+showAllThing()
+// console.log(number)// we can't access out side the value 
+
+
+// Block Scope: Block Scope is means, only for in side the {} value let or const by accessible. var don't follow block scope rule.
+
+if (age){
+    var totalAge = 20
+   
+
+} console.log(totalAge);
