a6c4aec69d72fe6f3c05a70e7f889155c050ba87
Sharif-38 <sharifulislam.tb@gmail.com>
2024-10-26 08:32:47 +0600

    add the other variable

--- 1.variable.js
+++ 1.variable.js
@@ -7,7 +7,6 @@
  * 1. If we declare the variable we must use let, var, const fist.
  * 2. Then variable name 
  * 3. Variable value.
- * 
  */
 let name
 console.log(name) // undefined
@@ -17,3 +16,21 @@ console.log(fistName) // Yusha bin Nun
 
 let age = 30
 console.log(`My name is ${fistName}. I am ${age} old`)
+
+// NamingConvension 
+
+/**
+ * 1. We can't use reserved keyword
+ * 2. we can't use fist latter capital letter and also number.
+ * 3. Meaningful should be variable name
+ * 4. Can't use space or hyphen
+ * 5. We can use camel-case 
+ * 6. We can use fist latter dollar sign.
+ */
+
+// let , var, const
+/**
+ * 1. var is old version
+ * 2. When we use let we can re-sing the value.
+ * 3. const we can't re-sign the value. 
+ */
